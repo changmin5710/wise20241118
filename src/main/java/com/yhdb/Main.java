5d76236cd25c48af192f8a5983d5ce0e4c4ed2f3
왕초보개발자 <im7kr@naver.com>
2024-12-17 00:32:28 +0900

    Scanner

--- src/main/java/com/yhdb/Main.java
+++ src/main/java/com/yhdb/Main.java
@@ -1,5 +1,7 @@
 package com.yhdb;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -14,6 +16,14 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        // 생성
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("(명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력된 명령어 : %s".formatted(cmd));
+
+        scanner.close();
     }
 }
